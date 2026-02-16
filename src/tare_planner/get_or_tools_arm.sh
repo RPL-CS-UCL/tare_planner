be4f0d027c44001be5a25752b9375efc4fdd9898
MZandtheRaspberryPi <mzandtheraspberrypi@gmail.com>
2026-02-16 18:01:45 +0000

    trying or-tools fix

---
+++ src/tare_planner/get_or_tools_arm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /home/$USERNAME && curl -L -o or-tools_arm64_debian-11_cpp_v9.8.3296.tar.gz https://github.com/google/or-tools/releases/download/v9.8/or-tools_arm64_debian-11_cpp_v9.8.3296.tar.gz
+tar -xzf or-tools_arm64_debian-11_cpp_v9.8.3296.tar.gz
+rm -rf /home/$USERNAME/ros_ws/src/exploration/tare_planner/src/tare_planner/or-tools/include /home/$USERNAME/ros_ws/src/tare_planner/src/exploration/tare_planner/or-tools/lib
+mv /home/$USERNAME/or-tools_aarch64_Debian-11_cpp_v9.8.3296/include /home/$USERNAME/ros_ws/src/exploration/tare_planner/src/tare_planner/or-tools
+mv /home/$USERNAME/or-tools_aarch64_Debian-11_cpp_v9.8.3296/lib /home/$USERNAME/ros_ws/src/exploration/tare_planner/src/tare_planner/or-tools
