f360218350f25dc66688261b2154b2bfcdbd0420
MZandtheRaspberryPi <mzandtheraspberrypi@gmail.com>
2026-02-17 11:49:02 +0000

    some updates to or-tools installation

--- src/tare_planner/get_or_tools_arm.sh
+++ src/tare_planner/get_or_tools_arm.sh
@@ -2,6 +2,6 @@
 
 cd /home/$USERNAME && curl -L -o or-tools_arm64_debian-11_cpp_v9.8.3296.tar.gz https://github.com/google/or-tools/releases/download/v9.8/or-tools_arm64_debian-11_cpp_v9.8.3296.tar.gz
 tar -xzf or-tools_arm64_debian-11_cpp_v9.8.3296.tar.gz
-rm -rf /home/$USERNAME/ros_ws/src/exploration/tare_planner/src/tare_planner/or-tools/include /home/$USERNAME/ros_ws/src/tare_planner/src/exploration/tare_planner/or-tools/lib
-mv /home/$USERNAME/or-tools_aarch64_Debian-11_cpp_v9.8.3296/include /home/$USERNAME/ros_ws/src/exploration/tare_planner/src/tare_planner/or-tools
-mv /home/$USERNAME/or-tools_aarch64_Debian-11_cpp_v9.8.3296/lib /home/$USERNAME/ros_ws/src/exploration/tare_planner/src/tare_planner/or-tools
+rm -rf /home/$USERNAME/ros_ws/src/exploration/tare_planner/src/tare_planner/or-tools/include /home/$USERNAME/ros_ws/src/exploration/tare_planner/src/tare_planner/or-tools/lib
+cp /home/$USERNAME/or-tools_aarch64_Debian-11_cpp_v9.8.3296/include /home/$USERNAME/ros_ws/src/exploration/tare_planner/src/tare_planner/or-tools/include
+cp /home/$USERNAME/or-tools_aarch64_Debian-11_cpp_v9.8.3296/lib /home/$USERNAME/ros_ws/src/exploration/tare_planner/src/tare_planner/or-tools/lib
